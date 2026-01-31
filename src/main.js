73f5a411fffb28deb5db12892909e9f0803f1f6e
nehab44 <nehab4@ad.unc.edu>
2026-01-31 14:48:17 -0500

    basic working audio player and model analysis

---
+++ src/main.js
@@ -0,0 +1,75 @@
+import { BasicPitch } from "@spotify/basic-pitch";
+import p5 from "p5";
+import * as tf from "@tensorflow/tfjs";
+
+new p5((p) => {
+
+  let audio;
+  let basicPitch;
+
+  p.setup = () => {
+    const input = p.createFileInput(displayType);
+    input.position(0, 100); 
+    console.log("ready!");
+  };
+
+
+  function displayType(file) {
+   p.text(`This is file's type is: ${file.type}`, 10, 10, 80, 80);
+   if (file.type == 'audio') {
+    audio = file
+    playSound()
+   }
+
+  }
+
+
+  async function resampleAudioBuffer(audioBuffer, targetSampleRate) {
+    const duration = audioBuffer.duration
+    
+    const offlineCtx = new OfflineAudioContext(
+      1, Math.ceil(duration * targetSampleRate), targetSampleRate
+    )
+
+    const source = offlineCtx.createBufferSource()
+    source.buffer = audioBuffer
+    source.connect(offlineCtx.destination)
+    source.start(0)
+
+    const resampledBuffer = await offlineCtx.startRendering();
+    return resampledBuffer
+
+  }
+
+
+  async function playSound() {
+    const arrayBuffer = await audio.file.arrayBuffer()
+    const context = new AudioContext()
+    const audioBuffer = await context.decodeAudioData(arrayBuffer)
+    const resampledBuffer = await resampleAudioBuffer(audioBuffer, 22050);
+    const source = context.createBufferSource();
+    source.buffer = audioBuffer
+    source.connect(context.destination)
+    source.start()
+
+    if (!basicPitch) {
+      const bpModel = await tf.loadGraphModel("/model/model.json")
+      basicPitch  = new BasicPitch(bpModel)
+    }
+    console.log(resampledBuffer.numberOfChannels)
+    const evaluate = await basicPitch.evaluateModel(
+      resampledBuffer,
+      (frames, onsets, contours) => {
+        console.log(frames) //duration of note
+        console.log(onsets) //when a new note begins
+        console.log(contours)
+      },
+      (percent) => {
+        console.log(Math.round(percent * 100))
+      }
+    
+    );
+    console.log(evaluate)
+  }
+
+});
